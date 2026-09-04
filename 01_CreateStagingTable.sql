2c83a18a1cdf76b988db530a730c0f6872abbf7b
SeanConnolly123 <seanconnolly9@live.co.uk>
2026-09-04 21:43:57 +0100

    Add files via upload
    In this code I created the staging table which is a vital part of the ETL process. It allowed me to clean the data without impacting the raw date, this is a key lesson I learn from my last project. It is imperative when creating the table that the data type of the column fits the raw data and the column names are the same, this allows me to insert the data in a bulk load from the raw table.

---
+++ 01_CreateStagingTable.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Sample_Superstore_Staging (
+			Row_ID FLOAT,
+			Order_ID VARCHAR(255),
+			Order_Date VARCHAR(255),
+			Ship_Date VARCHAR(255),
+			Ship_Mode VARCHAR(255),
+			Customer_ID VARCHAR(255),
+			Customer_Name VARCHAR(255),
+			Segment VARCHAR(255),
+			Country VARCHAR(255),
+			City VARCHAR(255),
+			State VARCHAR(255),
+			Postal_Code VARCHAR(255),
+			Region VARCHAR(255),
+			Product_ID VARCHAR(255),
+			Category VARCHAR(255),
+			Sub_Category VARCHAR(255),
+			Product_Name VARCHAR(255),
+			Sales FLOAT,
+			Quantity FLOAT,
+			Discount FLOAT,
+			Profit FLOAT
+			)
+
+
+
+
+
+
+
